06b9ae67afbab733d42ffcb5a9c01ca876ff4a25
Adishumla <45212964+Adishumla@users.noreply.github.com>
2023-10-28 23:54:26 +0200

    now the export of dashboard is fixed

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-export const DashboardPage: React.FC = () => {
+export default function Dashboard() {
   return (
     <div>
       <h1>Dashboard</h1>
       <p>Some dashboard content</p>
     </div>
   );
-};
+}
